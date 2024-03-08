18a400f68736de7bca692d552dbe4bbc9ee2662a
WFL173 <57353355+WFL173@users.noreply.github.com>
2024-03-08 17:15:22 +0300

    Implemented reading entire files for the lexing process.

--- src/main.c
+++ src/main.c
@@ -1,4 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define SC_OKAY(message, ...) printf("[+] " message "\n", ##__VA_ARGS__)
+#define SC_ERROR(message, ...) printf("[-] " message "\n", ##__VA_ARGS__)
+#define SC_WARNING(message, ...) printf("[!] " message "\n", ##__VA_ARGS__)
+#define SC_INFO(message, ...) printf("[I] " message "\n", ##__VA_ARGS__)
+
+typedef struct EntireFile
+{
+    char* Contents;
+    int Size; // this variable is equal to fileSize + 1. The extra one is from the '\0' character.
+} EntireFile;
+
+EntireFile ReadEntireFile(char* fileName)
+{
+    EntireFile result = {0};
+    FILE* file = fopen(fileName, "r");
+    if (!file)
+    {
+        SC_ERROR("file %s could not be found", fileName);
+        return result;
+    }
+    
+    fseek(file, 0, SEEK_END);
+    result.Size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    SC_INFO("file size %d", result.Size);
+    result.Contents = malloc(result.Size + 1);
+    fread(result.Contents, result.Size, 1, file);
+    result.Contents[result.Size++] = '\0';
+    fclose(file);
+
+    return result;
+}
 
 int main(int argc, char** argv[])
 {
