0dab2f8d8454a2d797132c8821d63086325a7542
WFL173 <57353355+WFL173@users.noreply.github.com>
2024-03-10 22:34:03 +0300

    Mapped single character token types to their ASCII values. Rearranged token types

--- src/token.h
+++ src/token.h
@@ -7,22 +7,52 @@ typedef enum
 {
     TOKEN_UNDEFINED = 0,
 
-    TOKEN_INT_LITERAL,
-    TOKEN_STRING_LITERAL,
+    // TODO: Put all single character tokens in ASCII table order here.
+    TOKEN_NOT = '!',
+    TOKEN_DOUBLE_QUOTE = '"',
+    TOKEN_HASH = '#',
+    TOKEN_MODULO = '%',
+    TOKEN_BITWISE_AND = '&',
+    TOKEN_SINGLE_QUOTE = '\'',
+
+    TOKEN_OPEN_PAREN = '(',
+    TOKEN_CLOSE_PAREN = ')',
+
+    TOKEN_MUL = '*',
+    TOKEN_PLUS = '+',
+    TOKEN_COMMA = ',',
+    TOKEN_MINUS = '-',
+    TOKEN_DOT = '.',
+    TOKEN_FORWARD_SLASH = '/',
+
+    TOKEN_COLON = ':',
+    TOKEN_SEMICOLON = ';',
+    TOKEN_LESS = '<',
+    TOKEN_EQUAL = '=',
+    TOKEN_BIGGER = '>',
+
+    TOKEN_OPEN_SQUARE_BRACKET = '[',
+    TOKEN_CLOSE_SQUARE_BRACKET = ']',
+
+    TOKEN_XOR = '^',
+
+    TOKEN_OPEN_BRACKET = '{',
+    TOKEN_BITWISE_OR = '|',
+    TOKEN_CLOSE_BRACKET = '}',
+
+    TOKEN_BITWISE_NOT = '~',
+
+    // TODO: Put all non single character tokens after this one
+    TOKEN_UNUSED = 128,
 
     TOKEN_KEYWORD,
     TOKEN_IDENTIFIER,
 
-    TOKEN_SEMICOLON,
-
-    TOKEN_OPEN_PAREN,
-    TOKEN_CLOSE_PAREN,
-
-    TOKEN_OPEN_BRACKET,
-    TOKEN_CLOSE_BRACKET,
-
-    TOKEN_OPEN_SQUARE_BRACKET,
-    TOKEN_CLOSE_SQUARE_BRACKET,
+    // TODO: Put literal token types here.
+    TOKEN_INT_LITERAL,
+    TOKEN_STRING_LITERAL,
+    TOKEN_FLOAT_LITERAL,
+    TOKEN_DOUBLE_LITERAL,
 
 } TokenType;
 
