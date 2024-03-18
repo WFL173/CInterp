67eb7fa63b3890089cd4833b3230e6580016d0b0
WFL173 <57353355+WFL173@users.noreply.github.com>
2024-03-18 16:55:59 +0300

    Added tokenization for keyword tokens. Rearranged some of the keyword processing logic.

--- src/lexer.c
+++ src/lexer.c
@@ -2,8 +2,9 @@
 #include "lexer.h"
 #include "log.h"
 #include "lexerCharTypes.h"
+#include "wordGraph.h"
 
-char* keywords[] =
+const char* keywords[] =
 {
     "return",
     "if",
@@ -11,6 +12,80 @@ char* keywords[] =
     "else",
     "while",
 };
+
+WordGraph keywordGraph = {0};
+
+void KeywordGraphInit()
+{
+    if (keywordGraph.Words)
+    {
+        return;
+    }
+
+    for (int i = 0; i < ARRAY_COUNT(keywords); i++)
+    {
+        WordGraphAddWord(&keywordGraph, WordFromString(keywords[i]));
+    }
+
+}
+
+void KeywordGraphFree()
+{
+    WordGraphFree(&keywordGraph);
+}
+
+int IsKeyword(const char* contents)
+{
+    int keywordFound = 0;
+
+    StringNodeQueue currentStates = {0};
+    StringNodeQueue nextStates = {0};
+    StringNodeQueue temp = {0};
+
+    for (int i = 0; i < keywordGraph.Size; i++)
+    {
+        StringNodeQueuePush(&currentStates, keywordGraph.Words[i].Start);
+        StringNodeQueuePush(&nextStates, keywordGraph.Words[i].Start);
+    }
+
+    for (int i = 0; IsIdentifier(contents[i]); i++)
+    {
+        char currentChar = contents[i];
+        
+        nextStates.Size = 0;
+        for (int j = 0; j < currentStates.Size; j++)
+        {
+            if (currentChar == currentStates.Nodes[j]->Key)
+            {
+                StringNodeQueuePush(&nextStates, currentStates.Nodes[j]->Next);
+            }   
+        }
+
+        temp = currentStates;
+        currentStates = nextStates;
+        nextStates = temp; 
+
+        if (currentStates.Size == 0)
+        {
+            break;
+        }
+          
+    }
+    
+    for (int i = 0; i < currentStates.Size; i++)
+    {
+        if (currentStates.Nodes[i]->Type == STRNODETYPE_END)
+        {
+            keywordFound = 1;
+            break;
+        }
+    }
+
+    StringNodeQueueFree(&currentStates);
+    StringNodeQueueFree(&nextStates);
+
+    return keywordFound;
+}
 
 Lexer LexerInit(const u8* contents, u64 contentSize)
 {
@@ -59,9 +134,48 @@ Token LexerReadNextToken(Lexer* lexer)
         return token;
     }
     
+   if (IsDigit(current))
+    {
+        u64 currentCursorPosition = lexer->Cursor;
+        s32 sum = 0;
+        while (IsDigit(lexer->Contents[lexer->Cursor]))
+        {
+            sum *= 10;
+            sum += lexer->Contents[lexer->Cursor] - '0';
+            lexer->Cursor++;
+        }
+        
+        SC_INFO("Integer literal %d found at line: %lld, colon: %lld", sum, lexer->Line, currentCursorPosition - lexer->BeginningOfLine);
+        token.Type = TOKEN_INT_LITERAL;
+        token.TokenData.IntValue = sum;
+        return token;
+    }
+
+    if (IsKeyword(&lexer->Contents[lexer->Cursor]))
+    {
+        u64 currentCursorPosition = lexer->Cursor++;
+        while (IsAlpha(lexer->Contents[lexer->Cursor]))
+        {
+            lexer->Cursor++;
+        }
+        u64 keywordSize = lexer->Cursor - currentCursorPosition;
+        u8* keyword = malloc(keywordSize + 1);
+
+        for (int i = 0; i < keywordSize; i++)
+        {
+            keyword[i] = lexer->Contents[currentCursorPosition + i];
+        }
+        keyword[keywordSize] = '\0';
+        
+        SC_INFO("keyword %s found at line: %lld, colon: %lld", keyword, lexer->Line, currentCursorPosition - lexer->BeginningOfLine);
+        token.TokenData.String = keyword;
+        token.Type = TOKEN_KEYWORD;
+        return token;
+    }
 
     if (IsIdentifierStart(current))
     {
+
         u64 currentCursorPosition = lexer->Cursor++;
         while (IsIdentifier(lexer->Contents[lexer->Cursor]))
         {
@@ -82,23 +196,6 @@ Token LexerReadNextToken(Lexer* lexer)
 
         return token;
     }
-
-    if (IsDigit(current))
-    {
-        u64 currentCursorPosition = lexer->Cursor;
-        s32 sum = 0;
-        while (IsDigit(lexer->Contents[lexer->Cursor]))
-        {
-            sum *= 10;
-            sum += lexer->Contents[lexer->Cursor] - '0';
-            lexer->Cursor++;
-        }
-        
-        SC_INFO("Integer literal %d found at line: %lld, colon: %lld", sum, lexer->Line, currentCursorPosition - lexer->BeginningOfLine);
-        token.Type = TOKEN_INT_LITERAL;
-        token.TokenData.IntValue = sum;
-        return token;
-    }
     
     lexer->Cursor++;
     return token;
@@ -106,7 +203,8 @@ Token LexerReadNextToken(Lexer* lexer)
 
 TokenArray LexerTokenize(Lexer* lexer)
 {
-    TokenArray result = TokenArrayInit();
+    TokenArray result = {0};
+    KeywordGraphInit();
 
     while(lexer->Cursor < lexer->ContentSize)
     {
@@ -114,6 +212,7 @@ TokenArray LexerTokenize(Lexer* lexer)
         TokenArrayAdd(&result, token);
     }
 
+    KeywordGraphFree();
     TokenArrayTrimToSize(&result);
     return result;
 }
